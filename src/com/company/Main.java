12250be69c989faf0a31b144ab955f9a382f0ed4
Raunak <rawnakkapoor@gmail.com>
2021-05-31 18:33:50 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Enter the Number: ");
+        Scanner Number = new Scanner(System.in);
+        System.out.println(Number.hasNextInt());    }
+}
